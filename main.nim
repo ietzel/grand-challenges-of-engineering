52e36ec73a4a733b2f21620f08ad6cb562a1a3e4
ietzel <67884277+ietzel@users.noreply.github.com>
2025-05-13 15:45:21 -0400

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,1 +1,4 @@
 echo "Through human history, technology has changed exponentially to meet needs."
+echo "Many past/present/future technologies have a cyclical relationship with each other."
+echo "Technological change has mixed environmental impacts—some positive and some negative, that are frequently hard to assess in the short run."
+echo "Meanwhile it has required increasing inputs of both private and public capital aimed to capturing technology rents that have promoted greater wealth inequalities."
