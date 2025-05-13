97c5ebb0f7dd55601300fccdef586c6e40e58dec
ietzel <67884277+ietzel@users.noreply.github.com>
2025-05-13 18:59:23 -0400

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,15 +1,13 @@
 echo 
-"
-Make Solar Energy Economical:
-Engineers are working to reduce the cost and improve the efficiency of solar energy technologies. 
-Provide Energy from Fusion:
-Developing sustainable and clean energy from fusion is a major long-term goal for engineers. 
-Develop Carbon Sequestration Methods:
-Capturing and storing excess carbon dioxide from the atmosphere is crucial for mitigating climate change. 
-Secure Cyberspace:
-Protecting critical infrastructure and data from cyberattacks is a growing concern for engineers. 
-Advance Health Informatics:
-Using data and technology to improve healthcare delivery and outcomes is a key area for engineering. 
-Engineer Better Medicines:
-Developing new and more effective treatments and therapies is a major focus for biomedical engineers. 
-"
+"Make Solar Energy Economical:"
+"Engineers are working to reduce the cost and improve the efficiency of solar energy technologies." 
+"Provide Energy from Fusion:"
+"Developing sustainable and clean energy from fusion is a major long-term goal for engineers." 
+"Develop Carbon Sequestration Methods:"
+"Capturing and storing excess carbon dioxide from the atmosphere is crucial for mitigating climate change." 
+"Secure Cyberspace:"
+"Protecting critical infrastructure and data from cyberattacks is a growing concern for engineers." 
+"Advance Health Informatics:"
+"Using data and technology to improve healthcare delivery and outcomes is a key area for engineering." 
+"Engineer Better Medicines:"
+"Developing new and more effective treatments and therapies is a major focus for biomedical engineers."
